09f29461deb583a8d3eb9d250dcde233309a94df
Grid <email@example.com>
2025-06-20 13:11:15 +0800

    修改反转

--- fw/bootloader/src/lcd_drv.c
+++ fw/bootloader/src/lcd_drv.c
@@ -65,8 +65,8 @@ void lcd_init(void) {
     lcd_write_command(0xe2); // 软复位
     delay(10);
     lcd_write_command(0xa2); // 升压电路,电压管理电路
-    lcd_write_command(0xa0); // 0x20~0x27 为V5电压内部电阻调整设置
-    lcd_write_command(0xc8); // 设置EV 对比度
+    lcd_write_command(0xa1); // 0x20~0x27 为V5电压内部电阻调整设置
+    lcd_write_command(0xc0); // 设置EV 对比度
     lcd_write_command(0x23); // 对比度值
 
     lcd_write_command(0x81); // 0x01~0x3f电量寄存器设置模式
@@ -75,7 +75,7 @@ void lcd_init(void) {
     lcd_write_command(0x2f); // 0xc0正向扫描, 0xc8反向扫描
     lcd_write_command(0xb0); // 0xa6正向显示, 0xa7反向显示
     lcd_write_command(0xAF); // 0xa4正常显示, 0xa5全屏显示
-    lcd_write_command(0xA6); // 背压比设置
+    lcd_write_command(0xA7); // 背压比设置
     // lcd_write_command(0x10);		//背压比值:0~10
     // lcd_write_command(0xaf);		//开显示
 #endif
